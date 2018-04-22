1e848a4b08a52a8c3eccfbf80fe7a2fbafb6fe76
mrousseaux <matthew@rousseaux.ca>
2018-04-22 15:52:20 -0600

    Initial Commit
    Imported block breakers menu system as a begining template.

---
+++ Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayerScript : MonoBehaviour {
+	static MusicPlayerScript instance;
+	void Awake () {
+		Debug.Log ("Music Player Awake: " + GetInstanceID());
+
+		if (instance != null) { //checks if an instance of this exists
+			Destroy (gameObject); //destroy's itself if an instance already exists
+			Debug.Log ("Music Player Copy Destroyed: " + GetInstanceID());
+		} else {
+			instance = this; //if instance is set to nothing, there is no instance. So set instance to this object.
+			GameObject.DontDestroyOnLoad (gameObject);	//don't destroy this object.
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Music Player Start: " + GetInstanceID());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
