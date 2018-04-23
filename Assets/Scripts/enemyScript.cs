dabdecb0a6c9e22bf59426af35c6465051d40a3d
mrousseaux <matthew@rousseaux.ca>
2018-04-22 21:48:16 -0600

    Enemy Hit Detection
    Added enemy hit detection and destruction

---
+++ Assets/Scripts/enemyScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyScript : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col){
+		Debug.Log ("Enemy Hit Detected");
+		Destroy (col.gameObject);
+		Destroy (gameObject);
+	}
+}
