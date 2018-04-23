7f85741d340b8bd9a5d5bed182f9b90a9245b0da
mrousseaux <matthew@rousseaux.ca>
2018-04-22 18:12:44 -0600

    Player Controller
    Added the player object and contoller script. left right control, and
    playerspace definition.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float shipSpeed = 10f;
+
+	float minX;
+	float maxX;
+	// Use this for initialization
+	void Start () {
+		float cameraDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftBoundery = Camera.main.ViewportToWorldPoint (new Vector3(0,0,cameraDistance));
+		Vector3 rightBoundery = Camera.main.ViewportToWorldPoint (new Vector3(1,0,cameraDistance));
+		minX = leftBoundery.x;
+		maxX = rightBoundery.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//player movement
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			//Move player left
+			transform.position += Vector3.left * shipSpeed * Time.deltaTime;
+		}else if (Input.GetKey (KeyCode.RightArrow)) {
+			//move player right
+			transform.position += Vector3.right * shipSpeed * Time.deltaTime;
+		}
+
+		//clamps player to the play space
+		float newShipX = Mathf.Clamp (transform.position.x, minX+0.5f, maxX-0.5f);
+		transform.position = new Vector3(newShipX, transform.position.y, transform.position.z); 
+
+	}
+}
---
+++ Assets/Sprites/EnemySpawnerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerScript : MonoBehaviour {
+	public GameObject enemyType;
+	// Use this for initialization
+	void Start () {
+		Instantiate (enemyType, new Vector3(0,0,0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
